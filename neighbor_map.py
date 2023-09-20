584f1acc6c19fe79c92a5a8e74027f327d059923
Arnaud Allera <45487966+arn-all@users.noreply.github.com>
2023-09-20 15:21:31 +0200

    Update neighbor_map.py
    Refactoring

--- neighbor_map.py
+++ neighbor_map.py
@@ -3,6 +3,25 @@ from scipy.spatial import KDTree
 import numpy as np
 import warnings
 
+
+class NeighborMap:
+
+    def __init__(self, atoms, gamma=2, beta=3) -> None:
+        self.gamma = gamma
+        self.beta = beta
+
+    @staticmethod
+    def pad(x: np.ndarray, image_size: int, target_size: int) -> np.ndarray:
+        """
+        Pad the input numpy array with zeroes on the right and bottom to match the target size.
+        x: Numpy array to be padded.
+        image_size: Current size of the image.
+        target_size: Desired size of the image.
+        Returns: Padded numpy array.
+        """
+        return np.pad(x, ((0, target_size-image_size), (0, target_size-image_size)))
+
+
 class Atoms(ase.Atoms):
     """
     Implements the get_descriptor() attribute to ase.Atoms. 
@@ -21,6 +40,7 @@ class Atoms(ase.Atoms):
         super().__init__(*args, **kwargs)
         self.wrap()
         self.tree = self.build_periodic_kdtree(self.positions, boxsize=self.cell.lengths())
+        self.neighbor_map = NeighborMap(self)
 
     @staticmethod
     def build_periodic_kdtree(positions, boxsize):
@@ -29,19 +49,8 @@ class Atoms(ase.Atoms):
         except ValueError:
           warnings.warn("Neighbor Map: Non cubic cell. Falling back to non-periodic implementation.")
           return KDTree(positions)
-        
-    @staticmethod
-    def pad(x: np.ndarray, image_size: int, target_size: int) -> np.ndarray:
-        """
-        Pad the input numpy array with zeroes on the right and bottom to match the target size.
-        x: Numpy array to be padded.
-        image_size: Current size of the image.
-        target_size: Desired size of the image.
-        Returns: Padded numpy array.
-        """
-        return np.pad(x, ((0, target_size-image_size), (0, target_size-image_size)))
 
-    def get_descriptor(self, atom_id: int, r_cut: float, img_target_size: int = 32) -> np.ndarray:
+    def get_neighbor_map(self, atom_id: int, r_cut: float, img_target_size: int = 32) -> np.ndarray:
         """
         Compute the descriptor for the atom at the given ID.
         atom_id: ID of the atom for which to compute the descriptor.
@@ -74,11 +83,13 @@ class Atoms(ase.Atoms):
 
         # Weights: [1, d_0j**m, ... ]
         img_size = min(number_atoms_in_rcut, img_target_size)
-        m = -1/3 * np.ones(img_size)
+
+
+        m = -1/self.neighbor_map.beta * np.ones(img_size)
         A = np.concatenate(([1], v[0, :-1]))**m
         
         # transform into a column vector so weights are applied row-wise
         A = A.reshape((-1,1))
 
         # pad with zeroes on the right and bottom
-        return self.pad(np.sqrt(A/v**2), img_size, img_target_size)
+        return self.neighbor_map.pad(A/v**self.neighbor_map.gamma, img_size, img_target_size)
